f78bf01b2d03d1ec9021585b0cc66dee647fbdf0
SCIPO666 <eaphoney@gmail.com>
2026-04-24 16:44:28 +0300

    db set up

--- server/app.js
+++ server/app.js
@@ -35,5 +35,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.listen(3000,'localhost',()=>{
+  console.log('app is live on local host 3000')
+})
 module.exports = app;
