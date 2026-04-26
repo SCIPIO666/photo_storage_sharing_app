ee729cd2cf994eccc10b45b27b67491dfe8b356d
Esphoney Ondicho <eaphoney@gmail.com>
2026-04-26 17:24:53 +0300

    fix(prisma): update cloned project setup for Prisma 7

--- server/config/prismaConfig.js
+++ server/config/prismaConfig.js
@@ -1,3 +1,8 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-module.exports = prisma;
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL
+})
+
+export default prisma
