5b4b87e63549fafb7f3a02439b1aab583a9e6f96
SCIPIO666 <eaphoney@gmail.com>
2026-04-28 14:09:43 +0300

    temprary front end view in ejs

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// 1. Home Page - The entry point "/"
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+// 2. Sign-Up Page - Renders the registration form
+router.get('/users/sign-up', function(req, res, next) {
+  res.render('signUp', { title: 'Create Account' });
+});
+
+// 3. Log-In Page - Renders the login form
+router.get('/users/log-in', function(req, res, next) {
+  res.render('logIn', { title: 'User Login' });
+});
+
+// 4. Uploads Page - Renders the multipart file form
+router.get('/uploads', function(req, res, next) {
+  res.render('uploads', { title: 'Upload Photos' });
 });
 
 module.exports = router;
